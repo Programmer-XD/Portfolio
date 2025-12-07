b36f113ee29d8fe8f177a95545a7a87841c31084
Melon <159301870+Programmer-XD@users.noreply.github.com>
2025-12-07 10:29:09 +0530

    Create providers.tsx

---
+++ src/app/providers.tsx
@@ -0,0 +1,12 @@
+import {HeroUIProvider} from '@heroui/react'
+
+export function Providers({children}: { children: React.ReactNode }) {
+  return (
+    <HeroUIProvider>
+      <main className="dark text-foreground bg-background">
+
+      {children}
+      </main>
+    </HeroUIProvider>
+  )
+}
